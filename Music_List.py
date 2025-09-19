8ccdeea8b855b2d5d356f9c57c46f7db1068dfa0
suryatejagandla-sys <suryatejagandla@gmail.com>
2025-09-19 18:10:08 +0530

    Add files via upload

---
+++ Music_List.py
@@ -0,0 +1,6 @@
+music = {
+    "jatt mannya": "https://youtu.be/DZYz4cnqF8A?si=mW_5vqf-3TI3Wdhe",
+    "chuttamalle": "https://youtu.be/5vsOv_bcnhs?si=gQDl9CTYohjjLTDl",
+    "waaliyan": "https://www.youtube.com/watch?v=fNhGuiKa8Jw",
+    "Ruby": "https://www.youtube.com/watch?v=B-9D96E2zPI"
+}
